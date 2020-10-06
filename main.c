9c5ca7087ce10f81ce3dd10272e408e9bf9093ce
naimkim <naimkim2000@naver.com>
2020-10-06 15:49:26 +0900

    02_again

--- main.c
+++ main.c
@@ -10,14 +10,12 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오  : ");
 	scanf("%i", &a);
 	
-	if (a>0)
-		printf("절대값은 %d 입니다. \n",a);
+	if (a<0)
+		a = a*(-1);
+	
+	printf("절대값은 %d 입니다. \n",a);
 		
-	else if (a<0)
-		printf("절대값은 %d 입니다. \n",-a);
-		
-	else 
-		printf("0입니다. \n ");
 	return 0;
+	
 }
 
